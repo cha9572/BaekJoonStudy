726c2d71453f5b34557035355be31837d8f504dd
cha9572 <cha9572@naver.com>
2026-01-30 22:35:17 +0900

    15652 : create baekjoon 15652

---
+++ BACKTRACKING/WEEK8_CKW/BOJ_15652/15652.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int N,M;
+int arr[9] = {0,};
+
+void dfs(int num, int count)
+{
+    if(count == M)
+    {
+        for(int i = 0; i < M; i++)
+            cout << arr[i] << ' ';
+        cout << '\n';
+        return;
+    }
+    for(int i = num; i <= N; i++)
+    {
+            arr[count] = i;
+            dfs(i,count+1);
+    }
+}
+
+int main() {
+    cin >> N >> M;
+    dfs(1,0);
+}
